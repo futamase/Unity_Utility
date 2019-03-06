76cfd555cc64619c0fae1f4a9449f01952d6bbf3
YukiFutamase <misoni.s.8989@gmail.com>
2019-03-06 15:48:30 +0900

    initial commit

---
+++ Assets/scripts/SingletonMonobehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonMonobehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    protected static T mInstance;
+    public static T Instance {
+        get {
+            if(mInstance == null) {
+				// 一応、すでに同じオブジェクトが存在しないか確認
+				// 検索コストが高いと感じるのであれば、この判定は消していい
+                mInstance = (T)FindObjectOfType(typeof(T));
+                if (mInstance == null) {
+					GameObject obj = new GameObject();
+                    mInstance = obj.AddComponent<T>();
+					
+					// 渡されたジェネリッククラス名を名前に出来る
+                    obj.name = mInstance.GetType().Name;
+					
+					// 破棄されたくないので、これを読んでおく
+                    DontDestroyOnLoad(obj);
+                }
+            }
+            return mInstance;
+        }
+    }
+}
